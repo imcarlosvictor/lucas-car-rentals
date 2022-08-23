d1d341a00492549bbf20ac26cef0c2b9c58524b8
Lucas <imcvlucas@gmail.com>
2022-08-23 03:47:24 -0400

    minor changes

--- cart/cart.py
+++ cart/cart.py
@@ -16,15 +16,15 @@ class Cart(object):
         self.cart = cart
 
     def add(self, product, quantity=1, override_quantity=False):
-        """Add a product to the cart or update its quantity."""
+        """Add a product to the cart"""
 
         product_id = str(product.id)
         if product_id not in self.cart:
-            self.cart[product_id] = {'quantity': 0, 'price': str(product.price)}
-        if override_quantity:
-            self.cart[product_id]['quantity'] = quantity
-        else:
-            self.cart[product_id]['quantity'] += quantity
+            self.cart[product_id] = {'quantity': 1, 'price': str(product.price)}
+        # if override_quantity:
+        #     self.cart[product_id]['quantity'] = quantity
+        # else:
+        #     self.cart[product_id]['quantity'] += quantity
         self.save()
 
     def save(self):
@@ -48,6 +48,7 @@ class Cart(object):
         cart = self.cart.copy()
         for product in products:
             cart[str(product.id)]['product'] = product
+            cart[str(product.id)]['brand'] = product.brand
         for item in cart.values():
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
