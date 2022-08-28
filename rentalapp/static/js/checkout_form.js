7cfc947ae61e09974a05108b049e70bbc7346d62
Lucas <imcvlucas@gmail.com>
2022-08-28 03:28:30 -0400

    Fix: Checkout page css complete

---
+++ rentalapp/static/js/checkout_form.js
@@ -0,0 +1,11 @@
+var form_fields = document.getElementsByTagName("input");
+form_fields[1].placeholder = "First Name";
+form_fields[2].placeholder = "Last Name";
+form_fields[3].placeholder = "Email";
+form_fields[4].placeholder = "Address";
+form_fields[5].placeholder = "Postal Code";
+form_fields[6].placeholder = "City";
+
+for (var fields in form_fields) {
+	form_fields[fields].className += "form-control";
+};
