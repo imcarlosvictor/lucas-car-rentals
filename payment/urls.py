7a125194fb98f254a7efb8f022c9a41f6f06aff5
Lucas <imcvlucas@gmail.com>
2022-09-21 20:00:24 -0400

    Orders marked as paid or not

--- payment/urls.py
+++ payment/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     # path('process/', views.payment_process, name='process'), # braintree
     path('done/', views.payment_done, name='done'),
     path('cancelled/', views.payment_cancelled, name='cancelled'),
-    path('checkout/', views.create_checkout_session, name='checkout'), # stripe
+    path('checkout/', views.checkout_view, name='checkout'), # stripe
     path('success/', views.checkout_success, name='success'),
 ]
--- payment/views.py
+++ payment/views.py
@@ -53,34 +53,42 @@ stripe.api_key = 'sk_test_51LhNbNBGZyNiyiqHLHFvN0Eucgo7IWOQcT33pjpIQ4CQd14nOvqHC
 
 DOMAIN = 'http://127.0.0.1:8000/'
 
-def create_checkout_session(request):
+def build_checkout_session(request):
     order_id = request.session.get('order_id')
     order = get_object_or_404(Order, id=order_id)
     total_cost = order.get_total_cost()
     total_cost_to_int = int(total_cost) * 100
 
-    try:
-        checkout_session = stripe.checkout.Session.create(
-            line_items=[{
-                'price_data': {
-                    'currency': 'cad',
-                    'product_data': {
-                        'name': 'T-shirt',
-                    },
-                    'unit_amount': total_cost_to_int,
+    checkout_session = stripe.checkout.Session.create(
+        line_items=[{
+            'price_data': {
+                'currency': 'cad',
+                'product_data': {
+                    'name': 'T-shirt',
                 },
-                'quantity': 1,
-            }],
-            mode='payment',
-            success_url= DOMAIN + 'payment/success/',
-            cancel_url= DOMAIN + 'payment/cancelled/',
-        )
-        order.paid = True
-        order.save()
-    except Exception as e:
-        return str(e)
+                'unit_amount': total_cost_to_int,
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url= DOMAIN + 'payment/success/',
+        cancel_url= DOMAIN + 'payment/cancelled/',
+    )
 
-    return redirect(checkout_session.url)
+    return checkout_session, order
+
+def checkout_view(request):
+    session, order = build_checkout_session(request)
+    return redirect(session.url)
 
 def checkout_success(request):
+    session, order = build_checkout_session(request)
+    order.paid = True
+    order.save()
+    return render(request, 'payment/success.html')
+
+def checkout_cancelled(request):
+    session, order = build_checkout_session(request)
+    order.paid = False
+    order.save()
     return render(request, 'payment/success.html')
