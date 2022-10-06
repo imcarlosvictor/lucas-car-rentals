f0c29767536c2becbc54e5722c4c72aa75eb5ef9
Lucas <imcvlucas@gmail.com>
2022-10-06 02:18:23 -0400

    Fix: Minor changes to settings.py

--- rentalproject/settings.py
+++ rentalproject/settings.py
@@ -26,7 +26,7 @@ CSRF_TRUSTED_ORIGINS = ['https://lucascarrentals.herokuapp.com/']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lucascarrentals.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
