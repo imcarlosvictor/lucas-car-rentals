28b3838471a956c3697c69af05057823f1d5c96d
Lucas <imcvlucas@gmail.com>
2022-10-07 02:38:41 -0400

    Refactor: minor changes

--- rentalproject/settings.py
+++ rentalproject/settings.py
@@ -152,8 +152,7 @@ CART_SESSION_ID = 'cart'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'noreply@lucascarrentals.com'
+EMAIL_HOST_USER = 'imcvlucas@gmail.com'
 EMAIL_HOST_PASSWORD = 'zbjtwtmipgeuobyd'
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'noreply@lucascarrentals.com'
 
